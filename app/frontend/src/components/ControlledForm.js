31d97a42dd4b7ad98f416da0618acc84cdb2630a
Sirinya Richardson <siri.richardson@gmail.com>
2023-11-17 11:29:56 +0100

    build: change form to functional component useState

--- app/frontend/src/components/ControlledForm.js
+++ app/frontend/src/components/ControlledForm.js
@@ -1,54 +1,53 @@
-import React, {Component} from 'react';
+import React, {useState} from 'react';
 
-class ControlledForm extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      fullName: '',
-      category: 'website',
-      comments: ''
-    }
-  }
+const ControlledForm = () => {
+  const {fullName, setName} = useState();
+  const {category, setCategory} = useState();
+  const {comments, setComments} = useState();
 
-  handleChange = e => {
-    const {name, value} = e.target
-    console.log(name, value)
-    this.setState({
-      [name]: value
-    })
-  }
-
-  handleSubmit = e => {
+  const handleSubmit = e => {
     e.preventDefault();
-    console.log(this.state.fullName, this.state.category, this.state.comments)
+    console.log(fullName, category, comments)
   }
 
-  render() {
-    return (
-      <div>
-        <h2>Please fill out the form below:</h2>
-        <form onSubmit={this.handleSubmit}>
-          <fieldset>
-            <label htmlFor="fullName">Your Name:</label>
-            <input name="fullName" id="fullName" type="text" value={this.state.fullName} onChange={this.handleChange}/>
-          </fieldset>
-          <fieldset>
-            <label htmlFor="category">Category</label>
-            <select name="category" id="category" value={this.state.category} onChange={this.handleChange}>
-              <option value="webiste">Website issue</option>
-              <option value="order">Order issue</option>
-              <option value="general">General inquiry</option>
-            </select>
-          </fieldset>
-          <fieldset>
-            <label htmlFor="comments">Comments</label>
-            <textarea name="comments" id="comments" cols="30" rows="10" value={this.state.comments} onChange={this.handleChange}></textarea>
-          </fieldset>
-          <button type="submit">Submit</button>
-        </form>
-      </div>
-    );
-  }
-}
+  return (
+    <div>
+      <h2>Please fill out the form below:</h2>
+      <form onSubmit={handleSubmit}>
+        <fieldset>
+          <label htmlFor="fullName">Your Name:</label>
+          <input
+            name="fullName"
+            id="fullName"
+            type="text" value={fullName}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </fieldset>
+        <fieldset>
+          <label htmlFor="category">Category</label>
+          <select
+            name="category"
+            id="category"
+            value={category}
+            onChange={(e) => setCategory(e.target.value)}>
+            <option value="webiste">Website issue</option>
+            <option value="order">Order issue</option>
+            <option value="general">General inquiry</option>
+          </select>
+        </fieldset>
+        <fieldset>
+          <label htmlFor="comments">Comments</label>
+          <textarea name="comments"
+                    id="comments"
+                    cols="30"
+                    rows="10"
+                    value={comments}
+                    onChange={(e) => setComments(e.target.value)}></textarea>
+        </fieldset>
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+};
 
 export default ControlledForm;
