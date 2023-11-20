57399792c51ef17af03862346028f9d26915580c
Sirinya Richardson <siri.richardson@gmail.com>
2023-11-20 16:38:56 +0100

    build: adjust the pronoun enums

--- backend/users/models.py
+++ backend/users/models.py
@@ -12,44 +12,46 @@ class Profile(models.Model):
     """
 
     class Pronouns(models.TextChoices):
+        NONE = "NOT", _("prefer not to say")
         FEMININE = "FEM", _("she/her")
         MASCULINE = "MAS", _("he/him")
         NEUTRAL = "NEU", _("they/them")
         NON_BINARY = "NON", _("xe/xem")
 
     user = models.OneToOneField(
-        verbose_name="user",
+        verbose_name="User",
         to=settings.AUTH_USER_MODEL,
         on_delete=models.CASCADE,
         related_name="user_profile"
     )
     location = models.CharField(
         max_length=255,
+        blank=True,
         null=True
     )
     pronoun = models.CharField(
         max_length=3,
         choices=Pronouns.choices,
-        default="Please select"
+        default=Pronouns.NONE
     )
     personal_website = models.URLField(
-        verbose_name="personal_website",
+        verbose_name="Personal website",
         blank=True,
         null=True
     )
     social_links = models.JSONField(
-        verbose_name="social_links",
+        verbose_name="Social links",
         blank=True,
         null=True
     )
     image = models.ImageField(
-        verbose_name="profile_image",
+        verbose_name="Profile image",
         upload_to="profile_image",
         blank=True,
         null=True
     )
     background_image = models.ImageField(
-        verbose_name="profile_background_image",
+        verbose_name="Background image",
         upload_to="profile_background_image",
         blank=True,
         null=True
