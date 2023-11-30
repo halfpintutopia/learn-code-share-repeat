31b93573fda8b9c6ccdf5fbafa168a70f574e239
Sirinya Richardson <siri.richardson@gmail.com>
2023-11-30 08:51:21 +0100

    style: add style to header and hero image

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,14 +1,58 @@
-import React from 'react';
 import {Link} from "react-router-dom";
-import Nav from "./Nav";
+import React, {useEffect, useState, useRef} from 'react';
+import logo from '../../public/media/images/logo-dark.svg';
+import "./sass/app.scss";
 
 const Header = () => {
+  const [modal, setModal] = useState(false);
+  const modalRef = useRef();
+  const showHideModal = () => {
+    setModal(!modal);
+  };
+
+  const handleClickOutside = (event) => {
+    if (event.target === modalRef.current) {
+      setModal(false);
+    }
+  };
+
+  useEffect(() => {
+    if (modal) {
+      modalRef.current.showModal();
+      document.body.addEventListener('mousedown', handleClickOutside);
+    } else {
+      modalRef.current.close();
+      document.body.removeEventListener('mousedown', handleClickOutside);
+    }
+    return () => {
+      document.body.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, [modal]);
+
   return (
-    <header>
-      <div className="bar">
-        <Link href="/">Logo</Link>
+    <header className="primary-header container" data-width="wide">
+      <div className="wrapper">
+        <div className="primary-header__inner">
+          <Link to="/">
+            <span className="sr-only">Home</span>
+            <img src={logo} alt="Logo for website"/>
+          </Link>
+          <nav>
+            <button onClick={showHideModal}>Open</button>
+          </nav>
+
+          <dialog ref={modalRef} className="nav-dialog">
+            <form method="dialog">
+              <label htmlFor="firstName">First Name</label>
+              <input id="firstName" name="firstName" type="text"/>
+              <button formMethod="dialog">cancel</button>
+              <button>Submit</button>
+            </form>
+            <div>This is not a dialog but a modal</div>
+            <button onClick={() => setModal(false)}>Close</button>
+          </dialog>
+        </div>
       </div>
-      <Nav/>
     </header>
   );
 };
