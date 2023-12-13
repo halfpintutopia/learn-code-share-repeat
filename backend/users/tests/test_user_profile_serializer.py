66e89dc60f54c1061de2b29b8994912f03278194
Sirinya Richardson <siri.richardson@gmail.com>
2023-12-13 11:27:18 +0100

    tests: add slug to serializer

--- backend/users/tests/test_user_profile_serializer.py
+++ backend/users/tests/test_user_profile_serializer.py
@@ -1,5 +1,7 @@
 import pytest
 
+from django.template.defaultfilters import slugify
+
 from users.models import Profile
 from users.serializers import UserProfileSerializer, UserSerializer
 
@@ -66,50 +68,17 @@ def test_user_serializer(user):
     assert serializer.data.get("last_name") == user.last_name
     assert serializer.data.get("email") == user.email
 
-# @pytest.mark.django_db
-# def test_update_user_profile_serializer(user, uploaded_image, uploaded_background_image):
-#     """
-#     GIVEN a valid user profile serializer
-#     WHEN the data is passed to the serializer using a mock request
-#     THEN the serializer should be valid
-#     """
-#     profile = Profile.objects.get(user=user)
-#     profile.location = "Switzerland"
-#     profile.pronoun = "she/her"
-#     profile.personal_website = "https://mywebsite.com"
-#     profile.social_links = {
-#         "LinkedIn": "https://linkedin/in/mellisasmythe"
-#     }
-#     profile.image = uploaded_image
-#     profile.background_image = uploaded_background_image
-#     profile.save()
-#
-#     data = {
-#         "personal_website": "https://newwebsite.com",
-#         "social_links": {
-#             "LinkedIn": "https://linkedin/user",
-#             "Facebook": "https://facebook.com/user"
-#         }
-#     }
-#     request = RequestFactory().get("/")
-#     request.user = user
-#     serializer = UpdateUserProfileSerializer(profile, data=data, context={"request": request})
-#
-#     assert serializer.is_valid(raise_exception=True)
-#
-#     serializer.save()
-#
-#     updated_profile = Profile.objects.get(user=user)
-#     updated_user = updated_profile.user
-#
-#     assert updated_user.username == user.username
-#     assert updated_user.first_name == user.first_name
-#     assert updated_user.last_name == user.last_name
-#     assert updated_user.email == user.email
-#     assert updated_profile.user_id == user.id
-#     assert updated_profile.location == profile.location
-#     assert updated_profile.pronoun == profile.pronoun
-#     assert updated_profile.personal_website == data["personal_website"]
-#     assert updated_profile.social_links == data["social_links"]
-#     assert updated_profile.image == profile.image
-#     assert updated_profile.background_image == profile.background_image
+
+@pytest.mark.django_db
+def test_user_profile_serializer_with_slug(user):
+    """
+    GIVEN a valid custom user serializer
+    WHEN the data is passed to the serializer
+    THEN the serializer should be valid and contain a slug
+    """
+    profile = Profile.objects.get(user=user)
+
+    serializer = UserProfileSerializer(profile)
+
+    assert serializer.data.get('slug') is not None
+    assert serializer.data.get("slug") == slugify(user.username)
