38532e3a6efc04fef61294a6d2f4fd9d5b7b8795
Sirinya Richardson <siri.richardson@gmail.com>
2023-12-13 11:40:33 +0100

    build: add login view

--- backend/users/views/users.py
+++ backend/users/views/users.py
@@ -1,10 +1,11 @@
 from django.contrib.auth import get_user_model
+from django.shortcuts import get_object_or_404
 
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
-from ..serializers import UserProfileSerializer
+from ..serializers import UserProfileSerializer, UserSerializer
 from ..models import Profile
 
 User = get_user_model()
@@ -13,7 +14,7 @@ User = get_user_model()
 # @route
 class ListUsers(APIView):
     """
-    View to get and update a user's profile
+    View to get all users' profiles
     """
     permission_classes = [
         IsAuthenticatedOrReadOnly
@@ -22,8 +23,26 @@ class ListUsers(APIView):
     @staticmethod
     def get(request):
         """
-        Get the user's profile details
+        Get all the users' profile details
         """
         profiles = Profile.objects.all()
         serializer = UserProfileSerializer(profiles, many=True)
+        return Response(serializer.data)
+
+
+class GetUpdateUserProfileView(APIView):
+    """
+    View to get and update user's profile
+    """
+    permission_classes = [
+        IsAuthenticatedOrReadOnly
+    ]
+
+    @staticmethod
+    def get(request, slug):
+        """
+        Get the user's profile details
+        """
+        profile = get_object_or_404(Profile, slug=slug)
+        serializer = UserSerializer(profile.user)
         return Response(serializer.data)
