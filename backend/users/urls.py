76c253933d709f49e6df5d5c1a9e4a2ae378fcad
Sirinya Richardson <siri.richardson@gmail.com>
2023-12-15 07:17:28 +0100

    build: add rest framework simple jwt for authenticating

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,9 +1,18 @@
 from django.urls import path
 
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView,
+)
+
 from .views.registration import RegisterUser, ActivateUser
 from .views.users import ListUsers, GetUpdateUserProfileView
 
 urlpatterns = [
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
     path("api/users", ListUsers.as_view(), name='list-users'),
     path("api/users/register", RegisterUser.as_view(), name='register-user'),
     path("api/users/activate/<uidb64>/<token>/", ActivateUser.as_view(), name='activate'),
