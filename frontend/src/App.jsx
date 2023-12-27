53350202a4a15b39be4273ce1f1b02355f505552
Sirinya Richardson <siri.richardson@gmail.com>
2023-12-27 19:12:24 +0100

    build: add Helmet

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HelmetProvider } from 'react-helmet-async';
 import "./components/sass/app.scss";
 import NotFound from "./components/NotFound";
 import ActivateUser from "./components/ActivateUser";
@@ -19,18 +20,20 @@ const queryClient = new QueryClient({
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <QueryClientProvider client={queryClient}>
-        <AuthProvider>
-          <Routes>
-            <Route path="/join/:type" element={<JoinPage />} />
-            <Route path="/activate/:uid/:token" element={<ActivateUser />} />
-            <Route path="/" element={<Homepage />} />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </AuthProvider>
-      </QueryClientProvider>
-    </BrowserRouter>
+    <HelmetProvider>
+      <BrowserRouter>
+        <QueryClientProvider client={queryClient}>
+          <AuthProvider>
+            <Routes>
+              <Route path="/join/:type" element={<JoinPage />} />
+              <Route path="/activate/:uid/:token" element={<ActivateUser />} />
+              <Route path="/" element={<Homepage />} />
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </AuthProvider>
+        </QueryClientProvider>
+      </BrowserRouter>
+    </HelmetProvider>
   );
 };
 
