a1355fa25233f359e7d050611438abbce630df1c
Sirinya Richardson <siri.richardson@gmail.com>
2023-12-29 21:12:48 +0100

    build: add assessment, feedback and video apps

--- backend/feedback/apps.py
+++ backend/feedback/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class VideoSharingConfig(AppConfig):
+class FeedbackConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'video_sharing'
+    name = 'feedback'
---
+++ backend/videos/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+# from django.contrib.auth.models import User
+#
+#
+# class Video(models.Model):
+#     """
+#
+#     """
+#     user = models.ForeignKey(User, on_delete=models.CASCADE)
+#     created_at = models.DateTimeField(auto_now_add=True)
+#     updated_at = models.DateTimeField(auto_now_add=True)
+#
