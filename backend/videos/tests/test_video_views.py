d3e72360f0da0aba36c7ad608729c3706ae11810
Sirinya Richardson <siri.richardson@gmail.com>
2023-12-29 23:34:32 +0100

    test: create video view

--- backend/videos/tests/test_video_views.py
+++ backend/videos/tests/test_video_views.py
@@ -23,9 +23,32 @@ def test_get_all_video_view(user, client, uploaded_image, uploaded_video):
         technology_versions="Python 3.9.0"
     )
 
-    url = reverse("list-video")
+    url = reverse("list-create-video")
     res = client.get(url)
 
     assert res.status_code == 200
     assert res.data[0]["user"] == user.username
     assert len(res.data) == 1
+
+
+@pytest.mark.django_db
+def test_create_video_view(authenticated_user, uploaded_image, uploaded_video):
+    """
+    GIVEN a video model
+    WHEN a user requests to create a video
+    THEN the user should get a video created
+    """
+    request, client, user = authenticated_user
+
+    url = reverse("list-create-video")
+    data = {
+        "image": uploaded_image,
+        "video": uploaded_video,
+        "title": "Test Video",
+        "technology_versions": "Python 3.9.0"
+    }
+    res = client.post(url, data=data)
+
+    assert res.status_code == 201
+    assert res.data["user"] == user.username
+    assert res.data["title"] == "Test Video"
