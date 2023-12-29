3d085ab66a689d1382c0daf76b08ac95206d3cb2
Sirinya Richardson <siri.richardson@gmail.com>
2023-12-29 22:32:19 +0100

    build: add video serializer

---
+++ backend/videos/serializers.py
@@ -0,0 +1,50 @@
+from django.contrib.auth import get_user_model
+from rest_framework import serializers
+
+from .models import Video
+
+User = get_user_model()
+
+
+class VideoSerializer(serializers.ModelSerializer):
+    """
+    Serializer for the Video model.
+    'user' field represents the associated username
+    """
+    user = serializers.ReadOnlyField(source='user.username')
+    is_owner = serializers.SerializerMethodField()
+    profile_user_id = serializers.ReadOnlyField(source='user.id')
+    profile_image = serializers.ReadOnlyField(source='user.profile.image.url')
+
+    def get_is_owner(self, obj):
+        """
+        Check if the current user is the owner of the video
+        """
+        request = self.context["request"]
+
+        if request and not request.user.is_anonymous:
+            return request.user == obj.user
+        return False
+
+    class Meta:
+        """
+        Fields to include in the serializer and read-only fields
+        """
+        model = Video
+        fields = [
+            "id",
+            "user",
+            "profile_user_id",
+            "profile_image",
+            "title",
+            "video",
+            "image",
+            "created_at",
+            "updated_at",
+            "is_owner"
+        ]
+        read_only_fields = [
+            'id',
+            "created_at",
+            "updated_at"
+        ]
