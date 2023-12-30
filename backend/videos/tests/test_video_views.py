936fb0a9abaf85202ed75d3ec162b909b882385c
Sirinya Richardson <siri.richardson@gmail.com>
2023-12-30 09:12:14 +0100

    tests: add retrieve and non authenticated user

--- backend/videos/tests/test_video_views.py
+++ backend/videos/tests/test_video_views.py
@@ -77,6 +77,7 @@ def test_get_single_video_view(user, client, uploaded_image, uploaded_video):
     assert res.data["user"] == user.username
     assert res.data["title"] == "Test Video"
 
+
 @pytest.mark.django_db
 def test_update_video_view(authenticated_user):
     """
@@ -156,3 +157,74 @@ def test_delete_video_view(authenticated_user, uploaded_image, uploaded_video):
 
     assert res.status_code == 204
     assert res.data is None
+
+
+@pytest.mark.django_db
+def test_update_video_view_with_non_authenticated_user(client, user):
+    """
+    GIVEN a video model
+    WHEN a user requests to update a video
+    THEN the user should get a video updated
+    """
+    uploaded_image = SimpleUploadedFile(
+        name='test_image.jpg',
+        content=open('videos/tests/image.jpg', 'rb').read(),
+        content_type='image/jpeg'
+    )
+    uploaded_video = SimpleUploadedFile(
+        name='test_video.mp4',
+        content=open('videos/tests/test_video.mp4', 'rb').read(),
+        content_type='video/mp4'
+    )
+    video = Video.objects.create(
+        user=user,
+        image=uploaded_image,
+        video=uploaded_video,
+        title="Test Video",
+        technology_versions="Python 3.9.0"
+    )
+
+    different_uploaded_image = SimpleUploadedFile(
+        name='different_test_image.jpg',
+        content=open('videos/tests/image.jpg', 'rb').read(),
+        content_type='image/jpeg'
+    )
+    different_uploaded_video = SimpleUploadedFile(
+        name='different_test_video.mp4',
+        content=open('videos/tests/video.mp4', 'rb').read(),
+        content_type='video/mp4'
+    )
+
+    url = reverse(
+        "get-update-delete-video",
+        kwargs={"pk": video.id}
+    )
+    data = {
+        "image": different_uploaded_image,
+        "video": different_uploaded_video,
+        "title": "Another Test Video",
+        "technology_versions": "Python 3.8.0"
+    }
+    res = client.put(
+        url,
+        data=data,
+        format='multipart'
+    )
+
+    assert res.status_code == 401
+
+
+@pytest.mark.django_db
+def test_retrieve_video_with_invalid_id(client, user):
+    """
+    GIVEN a video model
+    WHEN a user requests to get a video with an invalid id
+    THEN the user should get a 404 error
+    """
+    url = reverse(
+        "get-update-delete-video",
+        kwargs={"pk": 1}
+    )
+    res = client.get(url)
+
+    assert res.status_code == 404
