54a40d096f3ac1d3fb6ca4e106c965cba1bc2ec7
Sirinya Richardson <siri.richardson@gmail.com>
2024-01-08 19:16:13 +0100

    bugfix: edit CORS_ALLOW_ALL_ORIGINS and add worker

--- backend/app/settings.py
+++ backend/app/settings.py
@@ -135,14 +135,12 @@ else:
         r"^https://.*\.gitpod\.io$",
     ]
 
-CORS_ALLOW_ALL_ORIGINS = True
-CORS_ALLOW_CREDENTIALS = True
-
 # CORS_ALLOWED_ORIGINS = [
 #     "http://localhost:3000",  # React's default port
 # ]
 
 if DEBUG:
+    CORS_ALLOW_ALL_ORIGINS = True
     CORS_ALLOWED_CREDENTIALS = True
     CORS_ORIGIN_WHITELIST = (
         "http://localhost:8000",
