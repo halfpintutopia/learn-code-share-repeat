bdcffc24c460819c1ffba79c454ac786ffdbfcff
Sirinya Richardson <siri.richardson@gmail.com>
2024-02-11 20:16:18 +0100

    build: change api

--- frontend/src/constants/constants.js
+++ frontend/src/constants/constants.js
@@ -1,7 +1,7 @@
 const SERVER_URL = 'http://localhost:8000';
 
 const DEFAULT_BASE_URL = '/api';
-export const ACTIVATE_USER = `${SERVER_URL}/users/activate/`;
-export const REGISTER_USER = `${SERVER_URL}/users/register/`;
-export const TOKEN_OBTAIN_API = `${SERVER_URL}/auth/token/`;
-export const TOKEN_REFRESH_API = `${SERVER_URL}/auth/token/refresh/`;
+export const ACTIVATE_USER = `${SERVER_URL}${DEFAULT_BASE_URL}/users/activate/`;
+export const REGISTER_USER = `${SERVER_URL}${DEFAULT_BASE_URL}/users/register/`;
+export const TOKEN_OBTAIN_API = `${SERVER_URL}${DEFAULT_BASE_URL}/auth/token/`;
+export const TOKEN_REFRESH_API = `${SERVER_URL}${DEFAULT_BASE_URL}/auth/token/refresh/`;
