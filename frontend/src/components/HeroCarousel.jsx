fd913be57d21e3d4494ab025dcad8b482ad66425
Sirinya Richardson <siri.richardson@gmail.com>
2024-02-15 20:23:39 +0100

    build: add carousel component

--- frontend/src/components/HeroCarousel.jsx
+++ frontend/src/components/HeroCarousel.jsx
@@ -1,45 +1,57 @@
-import React from 'react';
-// import Demo from "../../public/media/images/computer_program.jpg";
-import Demo from "../media/images/computer_program.jpg";
 import { faChevronLeft, faChevronRight } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useState } from "react";
 
-const HeroCarousel = () => {
-  // const [ currentSlide, setCurrentSlide ] = useState(false);
-
-
+const HeroCarousel = ({ slides }) => {
+  const [ currentSlide, setCurrentSlide ] = useState(0);
+  const navigateSlide = (direction) => {
+    if (direction === 'next') {
+      setCurrentSlide(currentSlide === slides.length - 1 ? 0 : currentSlide + 1);
+    } else {
+      setCurrentSlide(currentSlide === 0 ? slides.length - 1 : currentSlide - 1);
+    }
+  };
   return (
-    <section>
-      <div className="carousel">
-        <button className="carousel__button carousel__button--left">
+    <>
+      <section className="carousel" aria-label="Newest Photos">
+        <button
+          className="carousel__button prev"
+          data-carousel-btn="prev"
+          onClick={ () => navigateSlide('prev') }
+        >
           <FontAwesomeIcon icon={ faChevronLeft }/>
         </button>
-        <div className="carousel__track-container">
-          <ul className="carousel__track">
-            {/*TODO loop videos*/ }
-            <li className="carousel__slide">
-              <img className="carousel__image" src={ Demo } alt="a test"/>
-              {/*<div className="carousel__slide--item">*/ }
-              {/*  <div className="item__overlay">*/ }
-              {/*    <h1 className="item__title">Code with Clarity & Confidence</h1>*/ }
-              {/*  </div>*/ }
-              {/*  <video className="item__background-media" autoPlay muted loop>*/ }
-              {/*    <source src={HeaderImage} type="video/webm" />*/ }
-              {/*  </video>*/ }
-              {/*</div>*/ }
-            </li>
-          </ul>
-        </div>
-        <button className="carousel__button carousel__button--right">
+        <button
+          className="carousel__button next"
+          data-carousel-btn="next"
+          onClick={ () => navigateSlide('next') }
+        >
           <FontAwesomeIcon icon={ faChevronRight }/>
         </button>
-
-        <div className="carousel__nav">
-          {/*TODO loop number of videos*/ }
-          {/*<button className="carousel__indicator" data-slide={ currentSlide }></button>*/}
+        <div className="carousel__wrapper">
+          <ul>
+            {
+              [ ...slides ].map((slide, index) => (
+                <li
+                  key={ index }
+                  className="carousel__slide"
+                  data-current-slide={ currentSlide === index ? 'true' : 'false' }
+                >
+                  {
+                    slide.mediaType === 'img' ? (
+                      <img src={ slide.mediaSrc } alt={ slide.altTag }/>
+                    ) : slide.mediaType === 'video' ? (
+                      // eslint-disable-next-line jsx-a11y/media-has-caption
+                      <video src={ slide.mediaSrc }></video>
+                    ) : null
+                  }
+                </li>
+              ))
+            }
+          </ul>
         </div>
-      </div>
-    </section>
+      </section>
+    </>
   );
 };
 
