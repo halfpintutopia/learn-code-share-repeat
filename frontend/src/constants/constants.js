b1d76044f84aa3fbddcda0d64c23a1c21703e214
Sirinya Richardson <siri.richardson@gmail.com>
2024-02-20 20:27:07 +0100

    bugfix: update the constants with new helper

--- frontend/src/constants/constants.js
+++ frontend/src/constants/constants.js
@@ -1,7 +1,7 @@
-const SERVER_URL = 'http://localhost:8000';
+import { createUrl } from "../helpers/api";
 
 const DEFAULT_BASE_URL = '/api';
-export const ACTIVATE_USER = `${SERVER_URL}${DEFAULT_BASE_URL}/users/activate/`;
-export const REGISTER_USER = `${SERVER_URL}${DEFAULT_BASE_URL}/users/register/`;
-export const TOKEN_OBTAIN_API = `${SERVER_URL}${DEFAULT_BASE_URL}/auth/token/`;
-export const TOKEN_REFRESH_API = `${SERVER_URL}${DEFAULT_BASE_URL}/auth/token/refresh/`;
+export const ACTIVATE_USER = createUrl(`${DEFAULT_BASE_URL}/users/activate/`);
+export const REGISTER_USER = createUrl(`${DEFAULT_BASE_URL}/users/register/`);
+export const TOKEN_OBTAIN_API = createUrl(`${DEFAULT_BASE_URL}/auth/token/`);
+export const TOKEN_REFRESH_API = createUrl(`${DEFAULT_BASE_URL}/auth/token/refresh/`);
