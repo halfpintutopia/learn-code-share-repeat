dd164f0c6bb270383be64985b8b31196f77c0a29
Sirinya Richardson <siri.richardson@gmail.com>
2024-02-29 21:12:46 +0100

    build: add and implement all auth accounts

--- backend/app/urls.py
+++ backend/app/urls.py
@@ -22,6 +22,7 @@ from django.views.generic import TemplateView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("api/api-auth/", include("rest_framework.urls")),
+    path("api/accounts/", include("allauth.urls")),
     path("api/", include("users.urls")),
     path("api/", include("videos.urls")),
     path("api/", include("feedback.urls")),
--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,3 +1,5 @@
+from allauth.account.views import PasswordResetFromKeyView
+from django.contrib.auth.views import PasswordResetView
 from django.urls import path
 
 from rest_framework_simplejwt.views import (
@@ -13,6 +15,9 @@ urlpatterns = [
     path('auth/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('auth/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('auth/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+    path('accounts/password/reset/', PasswordResetView.as_view(), name='account_password_reset'),
+    path('accounts/password/reset/key/<uidb64>/<token>/', PasswordResetFromKeyView.as_view(),
+         name='account_password_reset_from_key'),
     path("users/", ListUsers.as_view(), name='list-users'),
     path("users/register/", RegisterUser.as_view(), name='register-user'),
     path("users/activate/", ActivateUser.as_view(), name='activate'),
